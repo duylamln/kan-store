068adeef700e5ee3639e88d96ebfa9a7859750d7
Lam Le <Lam@formation.com.au>
2023-06-06 12:02:53 +1000

    edit store name

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from './Logo';
-import { Stack } from 'rsuite';
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "./Logo";
+import { Stack } from "rsuite";
 
 const Brand = props => {
   return (
     <Stack className="brand" {...props}>
       <Logo height={26} style={{ marginTop: 6 }} />
       <Link to="/">
-        <span style={{ marginLeft: 14 }}>Admin Template</span>
+        <span style={{ marginLeft: 14 }}>KAN Store</span>
       </Link>
     </Stack>
   );
