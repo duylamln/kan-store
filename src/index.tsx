beabc13a4e5b25ee9d9e11b7a883863203c555bb
Lam Le <Lam@formation.com.au>
2023-06-06 13:48:16 +1000

    test process evn

--- src/index.tsx
+++ src/index.tsx
@@ -13,12 +13,20 @@ import { IntlProvider } from "react-intl";
 import { CustomProvider } from "rsuite";
 import locales from "./locales";
 import enGB from "rsuite/locales/en_GB";
-const PARSE_APPLICATION_ID = "xN01HjcV6z6uqr8k1K4RIn08WaJ5UT3GrUrXtRb3";
-const PARSE_HOST_URL = "https://parseapi.back4app.com/";
-const PARSE_JAVASCRIPT_KEY = "pefuwnCnc58ge4wCwhLX0duMdgVCDm4xeF50tzzO";
 
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
+const REACT_APP_PARSE_APPLICATION_ID =
+  "xN01HjcV6z6uqr8k1K4RIn08WaJ5UT3GrUrXtRb3";
+const REACT_APP_PARSE_HOST_URL = "https://parseapi.back4app.com/";
+const REACT_APP_PARSE_JAVASCRIPT_KEY =
+  "pefuwnCnc58ge4wCwhLX0duMdgVCDm4xeF50tzzO";
+
+console.log("env: ", process?.env);
+
+Parse.initialize(
+  REACT_APP_PARSE_APPLICATION_ID,
+  REACT_APP_PARSE_JAVASCRIPT_KEY,
+);
+Parse.serverURL = REACT_APP_PARSE_HOST_URL;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
